87fc85c862af7e7988fb3abf1c177ca3fbcca360
Ankur Saxena <asaxena3@ncsu.edu>
2017-10-09 19:26:54 -0400

    Maven Project Created

---
+++ PulseBot/src/main/java/edu/ncsu/pulsebot/PulseBot/App.java
@@ -0,0 +1,13 @@
+package edu.ncsu.pulsebot.PulseBot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
