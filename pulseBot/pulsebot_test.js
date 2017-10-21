2bfc7ab82b420c10da242cd6208dfafc6f58ff58
jrane <jrane@ncsu.edu>
2017-10-21 00:43:40 -0400

    Update pulsebot_test.js

--- pulseBot/pulsebot_test.js
+++ pulseBot/pulsebot_test.js
@@ -2,6 +2,9 @@
 
 var Botkit = require('botkit');
 
+var redis = require('redis');
+client = redis.createClient();
+
 
 
 var controller = Botkit.slackbot({
@@ -15,12 +18,48 @@ controller.spawn({
   token: 'xoxb-259933162213-cuEXH2HjVx5ZEkR5BKnSJB3i',
 }).startRTM()
 
-controller.hears('bad commits',['mention', 'direct_mention','direct_message'], function(bot,message) 
+
+function checkIfUserExists(slackId){
+    client.hexists('userMap', slackId , function(err, reply){
+    // console.log("Reply is : " + reply);
+    return reply;
+	});
+}
+
+function adduser(slackId,githubId){
+    // console.log("slack id    "+ slackid);
+    client.hmset('userMap', slackId, githubId, function(err,reply){
+      console.log('{"GithubID ":"'+githubId+'"}');
+    });
+  }
+
+
+controller.hears('bad commits left',['mention', 'direct_mention','direct_message'], function(bot,message) 
 {
   console.log(message);
-  bot.reply(message,"You have 5 bad commits left.");
-
+  var reply = checkIfUserExists(message.user);//copy in all
+  if(reply==0)
+  	bot.reply(message,"Enter GitHubID followed by #");
+  else{
+  	client.hget('userMap', message.user, function(err, reply){
+  		client.hget('noOfBrokenCommits', reply, function(err, resp){
+  			bot.reply(message, "Number of Broken Commits: "+resp);
+  		});
+  	});
+  }
 });
+
+
+  controller.hears('#',['mention', 'direct_mention','direct_message'], function(bot,message) 
+	{
+	  console.log(message);
+	  var input = message.text;
+	  var gitID = input.split('#');
+	  var githubID = gitID[1];
+	  bot.reply(message,"Github ID: " + githubID);
+	  adduser(message.user, githubID);
+	});
+
 
 controller.hears('master branch status',['mention', 'direct_mention','direct_message'], function(bot,message) 
 {
@@ -35,3 +74,13 @@ controller.hears('stable branch name',['mention', 'direct_mention','direct_messa
   bot.reply(message,"The stable branch name is Bahubali");
 
 });
+
+controller.hears('stable branch name',['mention', 'direct_mention','direct_message'], function(bot,message) 
+{
+  console.log(message);
+  bot.reply(message,"The stable branch name is Bahubali");
+
+});
+
+
+
