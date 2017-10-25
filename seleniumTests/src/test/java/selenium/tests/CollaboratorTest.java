4eb7302bf867634953a84bdc9194b4c6c504a3b0
asaxena3 <asaxena3@ncsu.edu>
2017-10-25 07:43:37 +0000

    Add collaborator tests

---
+++ seleniumTests/src/test/java/selenium/tests/CollaboratorTest.java
@@ -0,0 +1,13 @@
+package selenium.tests;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CollaboratorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
