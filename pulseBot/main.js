c64436c17b1fed01c1be43bf30df72a92279ac0b
sshah11 <sshah11@ncsu.edu>
2017-10-26 00:29:13 +0000

    setting default testStable branch as master

--- pulseBot/main.js
+++ pulseBot/main.js
@@ -14,6 +14,9 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+var client = redis.client
+client.set("stableBranchNameTest", "master")
+
 app.post('/successBuild', function(req, res) {
   	const body = req.body
 
